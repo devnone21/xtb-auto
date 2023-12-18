94de24f2694c9ad149bed350f13a31cbe2ed9122
Thanet | Darnesh | Tiny <thanet.junlapon@gmail.com>
2023-12-18 20:26:49 +0700

    Restructure app.py

--- app.py
+++ app.py
@@ -2,82 +2,92 @@ from _init import *
 from XTBApi.api import Client
 from redis.exceptions import ConnectionError
 from datetime import datetime
-import pandas as pd
+from pandas import DataFrame
 LOGGER.setLevel(logging.DEBUG)
 
 
-def indicator_signal(client, symbol):
-    # get charts
-    now = int(datetime.now().timestamp())
-    res = client.get_chart_range_request(symbol, conf.period, now, now, -100)
-    digits = res['digits']
-    rate_infos = res['rateInfos']
-    LOGGER.info(f'recv {symbol} {len(rate_infos)} ticks.')
-    # caching
-    try:
-        cache = Cache()
-        for ctm in rate_infos:
-            cache.set_key(f'{symbol}_{conf.period}:{ctm["ctm"]}', ctm)
-        ctm_prefix = range(((now - 360_000) // 100_000), (now // 100_000)+1)
-        rate_infos = []
-        for pre in ctm_prefix:
-            mkey = cache.client.keys(pattern=f'{symbol}_{conf.period}:{pre}*')
-            rate_infos.extend(cache.get_keys(mkey))
-    except ConnectionError as e:
-        LOGGER.error(e)
-    # prepare candles
-    if not rate_infos:
-        return None, {}
-    rate_infos = [c for c in rate_infos if now - int(c['ctm'])/1000 > conf.period*60]
-    rate_infos.sort(key=lambda x: x['ctm'])
-    candles = pd.DataFrame(rate_infos)
-    candles['close'] = (candles['open'] + candles['close']) / 10 ** digits
-    candles['high'] = (candles['open'] + candles['high']) / 10 ** digits
-    candles['low'] = (candles['open'] + candles['low']) / 10 ** digits
-    candles['open'] = candles['open'] / 10 ** digits
-    LOGGER.info(f'got {symbol} {len(candles)} ticks.')
-    # evaluate
-    from signals import Fx
-    fx = Fx(algo=conf.algorithm, tech=conf.tech)
-    action, mode = fx.evaluate(candles)
-    epoch_ms = fx.candles.iloc[-1]['ctm']
-    return fx.candles, {"epoch_ms": epoch_ms, "action": action, "mode": mode}
+class Result:
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.market_status = False
+        self.df = DataFrame()
+        self.epoch_ms = 0
+        self.price = 0.0
+        self.action = ''
+        self.mode = ''
+
+    def get_signal(self, client=None):
+        # get charts
+        now = int(datetime.now().timestamp())
+        res = client.get_chart_range_request(self.symbol, conf.period, now, now, -100) if client else {}
+        digits = res.get('digits', 5)
+        rate_infos = res.get('rateInfos', [])
+        LOGGER.info(f'recv {self.symbol} {len(rate_infos)} ticks.')
+        # caching
+        try:
+            cache = Cache()
+            for ctm in rate_infos:
+                cache.set_key(f'{self.symbol}_{conf.period}:{ctm["ctm"]}', ctm)
+            ctm_prefix = range(((now - conf.period*60*400) // 100_000), (now // 100_000)+1)
+            rate_infos = []
+            for pre in ctm_prefix:
+                mkey = cache.client.keys(pattern=f'{self.symbol}_{conf.period}:{pre}*')
+                rate_infos.extend(cache.get_keys(mkey))
+        except ConnectionError as e:
+            LOGGER.error(e)
+        # prepare candles
+        if not rate_infos:
+            return None, {}
+        rate_infos = [c for c in rate_infos if now - int(c['ctm'])/1000 > conf.period*60]
+        rate_infos.sort(key=lambda x: x['ctm'])
+        candles = DataFrame(rate_infos)
+        candles['close'] = (candles['open'] + candles['close']) / 10 ** digits
+        candles['high'] = (candles['open'] + candles['high']) / 10 ** digits
+        candles['low'] = (candles['open'] + candles['low']) / 10 ** digits
+        candles['open'] = candles['open'] / 10 ** digits
+        LOGGER.info(f'got {self.symbol} {len(candles)} ticks.')
+        # evaluate
+        from signals import Fx
+        fx = Fx(algo=conf.algorithm, tech=conf.tech)
+        self.action, self.mode = fx.evaluate(candles)
+        self.df = fx.candles
+        self.price = self.df.iloc[-1]['close']
+        self.epoch_ms = self.df.iloc[-1]['ctm']
 
 
 def run():
     client = Client()
     client.login(conf.race_name, conf.race_pass, mode=conf.race_mode)
-    report = Notify()
     gcp = Cloud()
+    report = Notify()
     LOGGER.debug('Enter the Gate.')
 
     # Check if market is open
     market_status = client.get_market_status(conf.symbols)
     report.print_notify(f'[{conf.algorithm.upper()}_{conf.period}] Market status: {market_status}')
-    for symbol in market_status.keys():
-        if not market_status[symbol]:
+    for symbol, status in market_status.items():
+        if not status:
             continue
 
         # Market open, check signal
-        df, signal = indicator_signal(client, symbol)
-        if not signal:
+        r = Result(symbol)
+        r.market_status = status
+        r.get_signal(client=client)
+        if not r.action:
             continue
-        price = df.iloc[-1]['close']
-        action = signal.get("action")
-        mode = signal.get("mode")
-        ts = report.setts(datetime.fromtimestamp(int(signal.get("epoch_ms"))/1000))
-        report.print_notify(f'\nSignal: {symbol}, {ts}, {action}, {mode.upper()}, {price}')
-        LOGGER.debug(df.tail(2).head(1).iloc[:, [0, 1, -4, -3, -2, -1]].to_string(header=False))
-        LOGGER.debug(df.tail(1).iloc[:, [0, 1, -4, -3, -2, -1]].to_string(header=False))
+        ts = report.setts(datetime.fromtimestamp(int(r.epoch_ms)/1000))
+        report.print_notify(f'\nSignal: {symbol}, {ts}, {r.action}, {r.mode.upper()}, {r.price}')
+        LOGGER.debug(f'{symbol} - ' + r.df.tail(2).head(1).iloc[:, [0, 1, -4, -3, -2, -1]].to_string(header=False))
+        LOGGER.debug(f'{symbol} - ' + r.df.tail(1).iloc[:, [0, 1, -4, -3, -2, -1]].to_string(header=False))
 
         # Check signal to open/close transaction
-        if action.upper() in ('OPEN',):
-            res = trigger_open_trade(client, symbol=symbol, mode=mode)
-            report.print_notify(f'>> Open trade: {symbol} at {ts} of {conf.volume} with {mode.upper()}, {res}')
+        if r.action.upper() in ('OPEN',):
+            res = trigger_open_trade(client, symbol=symbol, mode=r.mode)
+            report.print_notify(f'>> Open trade: {symbol} at {ts} of {conf.volume} with {r.mode.upper()}, {res}')
             # gcp.pub(report.texts)
-        elif action.upper() in ('CLOSE',):
-            res = trigger_close_trade(client, symbol=symbol, mode=mode)
-            report.print_notify(f'>> Close opened trades: {symbol} at {ts} with {mode.upper()}, {res}')
+        elif r.action.upper() in ('CLOSE',):
+            res = trigger_close_trade(client, symbol=symbol, mode=r.mode)
+            report.print_notify(f'>> Close opened trades: {symbol} at {ts} with {r.mode.upper()}, {res}')
             # gcp.pub(report.texts)
 
     store_trade_rec(client, conf.race_name)
--- local_app.py
+++ local_app.py
@@ -1,45 +1,57 @@
 from _init import *
 from redis.exceptions import ConnectionError
 from datetime import datetime
-import pandas as pd
+from pandas import DataFrame
 LOGGER.setLevel(logging.DEBUG)
 
 
-def indicator_signal(symbol):
-    # get charts
-    now = int(datetime.now().timestamp())
-    digits = 5
-    rate_infos = []
-    LOGGER.info(f'recv {symbol} {len(rate_infos)} ticks.')
-    # caching
-    try:
-        cache = Cache()
-        for ctm in rate_infos:
-            cache.set_key(f'{symbol}_{conf.period}:{ctm["ctm"]}', ctm)
-        ctm_prefix = range(((now - 360_000) // 100_000), (now // 100_000)+1)
-        rate_infos = []
-        for pre in ctm_prefix:
-            mkey = cache.client.keys(pattern=f'{symbol}_{conf.period}:{pre}*')
-            rate_infos.extend(cache.get_keys(mkey))
-    except ConnectionError as e:
-        LOGGER.error(e)
-    # prepare candles
-    if not rate_infos:
-        return None, {}
-    rate_infos = [c for c in rate_infos if now - int(c['ctm'])/1000 > period*60]
-    rate_infos.sort(key=lambda x: x['ctm'])
-    candles = pd.DataFrame(rate_infos)
-    candles['close'] = (candles['open'] + candles['close']) / 10 ** digits
-    candles['high'] = (candles['open'] + candles['high']) / 10 ** digits
-    candles['low'] = (candles['open'] + candles['low']) / 10 ** digits
-    candles['open'] = candles['open'] / 10 ** digits
-    LOGGER.info(f'got {symbol} {len(candles)} ticks.')
-    # evaluate
-    from signals import Fx
-    fx = Fx(algo=conf.algorithm, tech=conf.tech)
-    action, mode = fx.evaluate(candles)
-    epoch_ms = fx.candles.iloc[-1]['ctm']
-    return fx.candles, {"epoch_ms": epoch_ms, "action": action, "mode": mode}
+class Result:
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.market_status = False
+        self.df = DataFrame()
+        self.epoch_ms = 0
+        self.price = 0.0
+        self.action = ''
+        self.mode = ''
+
+    def get_signal(self, client=None):
+        # get charts
+        now = int(datetime.now().timestamp())
+        res = client.get_chart_range_request(self.symbol, conf.period, now, now, -100) if client else {}
+        digits = res.get('digits', 5)
+        rate_infos = res.get('rateInfos', [])
+        LOGGER.info(f'recv {self.symbol} {len(rate_infos)} ticks.')
+        # caching
+        try:
+            cache = Cache()
+            for ctm in rate_infos:
+                cache.set_key(f'{self.symbol}_{conf.period}:{ctm["ctm"]}', ctm)
+            ctm_prefix = range(((now - conf.period*60*400) // 100_000), (now // 100_000)+1)
+            rate_infos = []
+            for pre in ctm_prefix:
+                mkey = cache.client.keys(pattern=f'{self.symbol}_{conf.period}:{pre}*')
+                rate_infos.extend(cache.get_keys(mkey))
+        except ConnectionError as e:
+            LOGGER.error(e)
+        # prepare candles
+        if not rate_infos:
+            return None, {}
+        rate_infos = [c for c in rate_infos if now - int(c['ctm'])/1000 > conf.period*60]
+        rate_infos.sort(key=lambda x: x['ctm'])
+        candles = DataFrame(rate_infos)
+        candles['close'] = (candles['open'] + candles['close']) / 10 ** digits
+        candles['high'] = (candles['open'] + candles['high']) / 10 ** digits
+        candles['low'] = (candles['open'] + candles['low']) / 10 ** digits
+        candles['open'] = candles['open'] / 10 ** digits
+        LOGGER.info(f'got {self.symbol} {len(candles)} ticks.')
+        # evaluate
+        from signals import Fx
+        fx = Fx(algo=conf.algorithm, tech=conf.tech)
+        self.action, self.mode = fx.evaluate(candles)
+        self.df = fx.candles
+        self.price = self.df.iloc[-1]['close']
+        self.epoch_ms = self.df.iloc[-1]['ctm']
 
 
 def run():
@@ -48,24 +60,22 @@ def run():
 
     for symbol in conf.symbols:
         # Market open, check signal
-        df, signal = indicator_signal(symbol)
-        if not signal:
+        r = Result(symbol)
+        r.get_signal()
+        if not r.action:
             continue
-        price = df.iloc[-1]['close']
-        action = signal.get("action")
-        mode = signal.get("mode")
-        ts = report.setts(datetime.fromtimestamp(int(signal.get("epoch_ms"))/1000))
-        report.print_notify(f'\nSignal: {symbol}, {ts}, {action}, {mode.upper()}, {price}')
-        LOGGER.debug(df.tail(2).head(1).iloc[:, [0, 1, -4, -3, -2, -1]].to_string(header=False))
-        LOGGER.debug(df.tail(1).iloc[:, [0, 1, -4, -3, -2, -1]].to_string(header=False))
-        
+        ts = report.setts(datetime.fromtimestamp(int(r.epoch_ms)/1000))
+        report.print_notify(f'\nSignal: {symbol}, {ts}, {r.action}, {r.mode.upper()}, {r.price}')
+        LOGGER.debug(f'{symbol} - ' + r.df.tail(2).head(1).iloc[:, [0, 1, -4, -3, -2, -1]].to_string(header=False))
+        LOGGER.debug(f'{symbol} - ' + r.df.tail(1).iloc[:, [0, 1, -4, -3, -2, -1]].to_string(header=False))
+
         # Check signal to open/close transaction
-        if action.upper() in ('OPEN',):
+        if r.action.upper() in ('OPEN',):
             res = "OK"
-            report.print_notify(f'>> Open trade: {symbol} at {ts} of {conf.volume} with {mode.upper()}, {res}')
-        if action.upper() in ('CLOSE',):
+            report.print_notify(f'>> Open trade: {symbol} at {ts} of {conf.volume} with {r.mode.upper()}, {res}')
+        elif r.action.upper() in ('CLOSE',):
             res = "OK"
-            report.print_notify(f'>> Close opened trades: {symbol} at {ts} with {mode.upper()}, {res}')
+            report.print_notify(f'>> Close opened trades: {symbol} at {ts} with {r.mode.upper()}, {res}')
 
 
 if __name__ == '__main__':
